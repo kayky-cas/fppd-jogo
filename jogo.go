3075c2e1e033b54e52004048c737d06050655c6d
Kayky Belleboni Casagrande <kbelleboni@gmail.com>
2025-04-21 22:21:37 -0300

    feat: inimigos agora seguem o player

--- jogo.go
+++ jogo.go
@@ -4,22 +4,24 @@ package main
 import (
 	"bufio"
 	"os"
+	"sync"
 )
 
 // Elemento representa qualquer objeto do mapa (parede, personagem, vegetação, etc)
 type Elemento struct {
-	simbolo   rune
-	cor       Cor
-	corFundo  Cor
-	tangivel  bool // Indica se o elemento bloqueia passagem
+	simbolo  rune
+	cor      Cor
+	corFundo Cor
+	tangivel bool // Indica se o elemento bloqueia passagem
 }
 
 // Jogo contém o estado atual do jogo
 type Jogo struct {
-	Mapa            [][]Elemento // grade 2D representando o mapa
-	PosX, PosY      int          // posição atual do personagem
-	UltimoVisitado  Elemento     // elemento que estava na posição do personagem antes de mover
-	StatusMsg       string       // mensagem para a barra de status
+	Mapa           [][]Elemento // grade 2D representando o mapa
+	PosX, PosY     int          // posição atual do personagem
+	UltimoVisitado Elemento     // elemento que estava na posição do personagem antes de mover
+	StatusMsg      string       // mensagem para a barra de status
+	mutex          sync.Mutex
 }
 
 // Elementos visuais do jogo
@@ -27,7 +29,7 @@ var (
 	Personagem = Elemento{'☺', CorCinzaEscuro, CorPadrao, true}
 	Inimigo    = Elemento{'☠', CorVermelho, CorPadrao, true}
 	Parede     = Elemento{'▤', CorParede, CorFundoParede, true}
-	Vegetacao  = Elemento{'♣', CorVerde, CorPadrao, false}
+	Vegetacao  = Elemento{'♣', CorVerde, CorPadrao, true}
 	Vazio      = Elemento{' ', CorPadrao, CorPadrao, false}
 )
 
@@ -102,7 +104,7 @@ func jogoMoverElemento(jogo *Jogo, x, y, dx, dy int) {
 	// Obtem elemento atual na posição
 	elemento := jogo.Mapa[y][x] // guarda o conteúdo atual da posição
 
-	jogo.Mapa[y][x] = jogo.UltimoVisitado     // restaura o conteúdo anterior
-	jogo.UltimoVisitado = jogo.Mapa[ny][nx]   // guarda o conteúdo atual da nova posição
-	jogo.Mapa[ny][nx] = elemento              // move o elemento
+	jogo.Mapa[y][x] = jogo.UltimoVisitado   // restaura o conteúdo anterior
+	jogo.UltimoVisitado = jogo.Mapa[ny][nx] // guarda o conteúdo atual da nova posição
+	jogo.Mapa[ny][nx] = elemento            // move o elemento
 }
--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 // main.go - Loop principal do jogo
 package main
 
-import "os"
+import (
+	"math"
+	"os"
+	"time"
+)
+
+const (
+	Cima     = 1
+	Baixo    = 2
+	Esquerda = 4
+	Direita  = 8
+)
+
+type Evento int
+
+const (
+	EventoSair Evento = iota
+	EventoDesenha
+	EventoDerrota
+)
 
 func main() {
 	// Inicializa a interface (termbox)
@@ -23,12 +42,137 @@ func main() {
 	// Desenha o estado inicial do jogo
 	interfaceDesenharJogo(&jogo)
 
+	eventCh := make(chan Evento)
+
+	go moveJogador(&jogo, eventCh)
+	prepararInimigos(&jogo, eventCh)
+
 	// Loop principal de entrada
+	for event := range eventCh {
+		switch event {
+		case EventoSair:
+			return
+		case EventoDesenha:
+			jogo.mutex.Lock()
+			interfaceDesenharJogo(&jogo)
+			jogo.mutex.Unlock()
+			break
+		case EventoDerrota:
+			return
+		}
+	}
+}
+
+func prepararInimigos(jogo *Jogo, eventCh chan Evento) {
+	for j, linha := range jogo.Mapa {
+		for i, elemento := range linha {
+			if elemento.simbolo == '☠' {
+				go seguirPlayer(jogo, i, j, eventCh)
+			}
+		}
+	}
+}
+
+func seguirPlayer(jogo *Jogo, i, j int, eventCh chan Evento) {
+	var direcoes int
+
 	for {
-		evento := interfaceLerEventoTeclado()
-		if continuar := personagemExecutarAcao(evento, &jogo); !continuar {
+		time.Sleep(1000 * time.Millisecond)
+
+		direcoes = 0
+
+		for {
+			if direcoes == Cima|Baixo|Direita|Esquerda {
+				return
+			}
+
+			jogo.mutex.Lock()
+			playerX, playerY := jogo.PosX, jogo.PosY
+
+			jogo.mutex.Unlock()
+			nextPosX, nextPosY := proximaAcao(playerX, playerY, i, j, &direcoes)
+			jogo.mutex.Lock()
+
+			if jogo.PosX == nextPosX && jogo.PosY == nextPosY {
+				eventCh <- EventoDerrota
+				return
+			}
+
+			elemento := jogo.Mapa[nextPosY][nextPosX]
+
+			if elemento.tangivel {
+				jogo.mutex.Unlock()
+				continue
+			}
+
+			jogo.Mapa[nextPosY][nextPosX] = jogo.Mapa[j][i]
+			jogo.Mapa[j][i] = elemento
+
+			jogo.mutex.Unlock()
+
+			i, j = nextPosX, nextPosY
+
+			eventCh <- EventoDesenha
+
 			break
 		}
-		interfaceDesenharJogo(&jogo)
+	}
+}
+
+func proximaAcao(playerX, playerY, inimigoX, inimigoY int, direcoes *int) (int, int) {
+	dx := playerX - inimigoX
+	dy := playerY - inimigoY
+
+	absDx := math.Abs(float64(dx))
+	absDy := math.Abs(float64(dy))
+
+	moveX := func() bool {
+		if dx > 0 && *direcoes&Direita == 0 {
+			*direcoes |= Direita
+			inimigoX += 1
+			return true
+		} else if dx < 0 && *direcoes&Esquerda == 0 {
+			*direcoes |= Esquerda
+			inimigoX -= 1
+			return true
+		}
+		return false
+	}
+
+	moveY := func() bool {
+		if dy > 0 && *direcoes&Baixo == 0 {
+			*direcoes |= Baixo
+			inimigoY += 1
+			return true
+		} else if dy < 0 && *direcoes&Cima == 0 {
+			*direcoes |= Cima
+			inimigoY -= 1
+			return true
+		}
+		return false
+	}
+
+	if absDx > absDy {
+		if !moveX() {
+			moveY()
+		}
+	} else {
+		if !moveY() {
+			moveX()
+		}
+	}
+
+	return inimigoX, inimigoY
+}
+
+func moveJogador(jogo *Jogo, eventCh chan<- Evento) {
+	for {
+		evento := interfaceLerEventoTeclado()
+		jogo.mutex.Lock()
+		if continuar := personagemExecutarAcao(evento, jogo); !continuar {
+			eventCh <- EventoSair
+		}
+		jogo.mutex.Unlock()
+		eventCh <- EventoDesenha
 	}
 }
